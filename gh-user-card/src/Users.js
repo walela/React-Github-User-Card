ac48cf8ea073ac32ab59ab80e0091981d208ba2c
walela <austinmasibo@gmail.com>
2020-02-12 16:15:56 +0300

    Rendered components

--- gh-user-card/src/Users.js
+++ gh-user-card/src/Users.js
@@ -1,6 +1,6 @@
 import React from "react"
 import axios from "axios"
-import UserCard from "./UserCard"
+import UserCard from "./Card"
 
 class Users extends React.Component {
   componentDidMount() {
